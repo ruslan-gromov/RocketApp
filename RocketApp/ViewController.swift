b47bd567f68e796fb7819b376e8817ca14d30bbd
Ruslan Gromov <ruslan.gromov@icloud.com>
2023-03-05 15:55:50 +0300

    App Project
    code inserted in Xcode project

--- RocketApp/ViewController.swift
+++ RocketApp/ViewController.swift
@@ -11,7 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        let url = URL(string: "https://api.spacexdata.com/v4/rockets")!
+
+        var request = URLRequest(url: url)
+
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                if let rockets = try? JSONDecoder().decode([Rocket].self, from: data) {
+                    print(rockets[0])
+                } else {
+                    print("Invalid Response")
+                }
+            } else if let error = error {
+                print("HTTP Request Failed \(error)")
+            }
+        }
+
+        task.resume()
+        
+        
     }
 
 
