4159b3f855ad70efd0a83e5b8f0f2bc988497d5c
Ruslan Gromov <ruslan.gromov@icloud.com>
2023-03-09 13:55:39 +0300

    upd 1355

--- RocketApp/Controllers/GetRemoteDataController.swift
+++ RocketApp/Controllers/GetRemoteDataController.swift
@@ -1,126 +1,90 @@
 import UIKit
 
-public class PickFlavorViewController: UIViewController {
-  // MARK: - Instance Properties
-  public var flavors: [Flavor] = []
-  private let flavorFactory = FlavorFactory()
+public class Rocket: Decodable {
+    
+    private let id: String
+    
+    public let name: String
+    public let type: String
+    public let active: Bool
+    
+    public let stages: Int
+    public let boosters: Int
+    public let costPerLaunch: Int
+    
+    public let firstFlight: String
+    public let country: String
+    
+    public let flickrImages: [String]
+    
+    internal let height: LocalLength
+    internal let diameter: LocalLength
+    internal let mass: LocalWeight
+        
+    
+    private func loadRemoteData() -> String {
 
-  // MARK: - Outlets
-  @IBOutlet var contentView: UIView!
-  @IBOutlet var collectionView: UICollectionView!
-//  @IBOutlet var iceCreamView: IceCreamView!
-  @IBOutlet var label: UILabel!
+        let requestUrl = URL(string: "https://api.spacexdata.com/v4/rockets")!
+        var request = URLRequest(url: requestUrl)
+        var result: String
 
-  // MARK: - View Lifecycle
-  public override func viewDidLoad() {
-    super.viewDidLoad()
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = URLSession.shared.dataTask(with: requestUrl) { data, response, error in
+            if let data = data {
+                if let rockets = try? JSONDecoder().decode([Rocket].self, from: data) {
+                    result = "sdsdfd"
+                } else {
+                   // return "Invalid Response"
+                }
+            } else if let error = error {
+                // return "HTTP Request Failed \(error)"
+            }
+        }
 
-    loadFlavors()
-  }
+        task.resume()
 
-  private func loadFlavors() {
-    // TO-DO: Implement this
-  }
+        return result
 
-  private func selectFirstFlavor() {
-    guard let flavor = flavors.first else {
-      return
     }
-    update(with: flavor)
-  }
-}
-
-// MARK: - FlavorAdapter
-extension PickFlavorViewController: FlavorAdapter {
-  public func update(with flavor: Flavor) {
-//    iceCreamView.update(with: flavor)
-    label.text = flavor.name
-  }
-}
-
-// MARK: - UICollectionViewDataSource
-extension PickFlavorViewController: UICollectionViewDataSource {
-  private struct CellIdentifiers {
-    static let scoop = "ScoopCell"
-  }
-
-  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return flavors.count
-  }
-
-  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.scoop, for: indexPath) as! ScoopCell
-    let flavor = flavors[indexPath.row]
-    cell.update(with: flavor)
-    return cell
-  }
-}
-
-// MARK: - UICollectionViewDelegate
-extension PickFlavorViewController: UICollectionViewDelegate {
-  public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    let flavor = flavors[indexPath.row]
-    update(with: flavor)
-  }
+    
+    
+    
+    
 }
 
 
+public class Launch: Decodable {
+    
+    private let id: String
+    private let launchLibraryId: String?
+    
+    public let name: String
+    
+    private let rocket: String
+    public let success: Bool
+    
+    public let details: String
+    public let staticFireDateUnix: Int
+    
+    private func loadRemoteData() {
 
+        let requestUrl = URL(string: "https://api.spacexdata.com/v4/rockets")!
+        var request = URLRequest(url: requestUrl)
 
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let task = URLSession.shared.dataTask(with: requestUrl) { data, response, error in
+            if let data = data {
+                if let rockets = try? JSONDecoder().decode([Rocket].self, from: data) {
+                    print(rockets[0])
+                } else {
+                    print("Invalid Response")
+                }
+            } else if let error = error {
+                print("HTTP Request Failed \(error)")
+            }
+        }
+        task.resume()
+    }
 
-struct Rocket: Decodable {
-
-    // describing simple props
-    
-    let id: String
-    
-    let name: String
-    let type: String
-    let active: Bool
-    // to short up print result -> let description: String
-    
-    let stages: Int
-    let boosters: Int
-    let cost_per_launch: Int
-    
-    let first_flight: String
-    let country: String
-    
-    let flickr_images: [String]
-    
-    // describing complex props
-    
-    struct LocalLength: Decodable {
-        let meters: Double
-        let feet: Double
-    }
-    
-    struct LocalWeight: Decodable {
-        let kg: Int
-        let lb: Int
-    }
-    
-    let height: LocalLength
-    let diameter: LocalLength
-    let mass: LocalWeight
-    
-    struct LocalThrust: Decodable {
-        let kN: Int
-        let lbf: Int
-    }
-    
-    // is it correct?
-    struct Stages: Decodable {
-        let thrust_sea_level: LocalThrust
-        let thrust_vacuum: LocalThrust
-        let reusable: Bool
-        let engines: Int
-        let fuel_amount_tons: Int
-        let burn_time_sec: Int? // can have nil value
-    }
-    
-    // some bug here...
-    // let first_stage: Stages
     
 }
-
